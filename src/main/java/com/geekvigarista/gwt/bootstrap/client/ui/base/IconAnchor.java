b3b289b795ea3c2f5c0afd25a3d0c28bd9b150a4
Carlos Alexandro Becker <caarlos0@gmail.com>
2012-03-22 00:53:41 -0300

    added support to white icons in uibinder.

--- src/main/java/com/geekvigarista/gwt/bootstrap/client/ui/base/IconAnchor.java
+++ src/main/java/com/geekvigarista/gwt/bootstrap/client/ui/base/IconAnchor.java
@@ -5,35 +5,38 @@ import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.HasText;
 
-
 public class IconAnchor extends ComplexWidget implements HasText {
 
-    private String text;
-    
-    public IconAnchor() {
-    	
-    	super("a");
-        getElement().appendChild(Document.get().createElement("i"));
-    }
+	private String text;
 
-    public void setIcon(Icon icon) {
-        if (icon == null) {
-            return;
-        }
-        setIcon(icon.getStyleName());
-    }
-    
-    public void setIcon(String iconname) {
-    	getElement().getElementsByTagName("i").getItem(0).setClassName(
-    			"icon-" + iconname);
-    }
+	public IconAnchor() {
 
-    public void setText(String text) {
-    	this.text = text;
-        Element i = getElement().getElementsByTagName("i").getItem(0);
-        getElement().setInnerHTML((i != null ? i.toString() : "<i></i>") + 
-        		" " + text);
-    }
+		super("a");
+		getElement().appendChild(Document.get().createElement("i"));
+	}
+
+	public void setIcon(Icon icon) {
+		if (icon == null) {
+			return;
+		}
+		setIcon(icon.getStyleName());
+	}
+
+	public void setIcon(String iconname) {
+		String[] iconnames = iconname.split(" ");
+		String output = "";
+		for (String s : iconnames) {
+			output += (" icon-" + s);
+		}
+		getElement().getElementsByTagName("i").getItem(0).setClassName(output);
+	}
+
+	public void setText(String text) {
+		this.text = text;
+		Element i = getElement().getElementsByTagName("i").getItem(0);
+		getElement().setInnerHTML(
+				(i != null ? i.toString() : "<i></i>") + " " + text);
+	}
 
 	public String getText() {
 		return text;
