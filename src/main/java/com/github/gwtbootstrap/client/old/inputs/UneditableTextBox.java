5bac53d2233fed8fa902f3ce273fb22c0289de3f
Carlos Alexandro Becker <caarlos0@gmail.com>
2012-03-22 21:43:29 -0300

    remove the `old` pkg; make necessary changes due to package rename

--- src/main/java/com/github/gwtbootstrap/client/old/inputs/UneditableTextBox.java
+++
@@ -1,9 +0,0 @@
-package com.github.gwtbootstrap.client.old.inputs;
-
-import com.github.gwtbootstrap.client.ui.resources.Bootstrap;
-
-public class UneditableTextBox extends TextBox {
-	{
-		addStyleName(Bootstrap.uneditable_input);
-	}
-}
