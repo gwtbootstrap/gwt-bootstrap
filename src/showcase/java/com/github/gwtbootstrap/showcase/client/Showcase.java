81e71aea7d906918dcc1147154285a7b33d350fa
soundTricker <keisuke.oohashi@gmail.com>
2012-04-01 19:14:57 +0900

    move facebook script

--- src/showcase/java/com/github/gwtbootstrap/showcase/client/Showcase.java
+++ src/showcase/java/com/github/gwtbootstrap/showcase/client/Showcase.java
@@ -61,7 +61,7 @@ public class Showcase extends Composite implements EntryPoint {
 		doc.getBody().appendChild(script);
 		
 		JavaScriptInjector.inject("!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=\"//platform.twitter.com/widgets.js\";fjs.parentNode.insertBefore(js,fjs);}}(document,\"script\",\"twitter-wjs\");");
-
+		JavaScriptInjector.inject("(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = \"//connect.facebook.net/en_US/all.js#xfbml=1\";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));");
 	}
 
 	private void addSectionToContainer(String sectionName, String target,
