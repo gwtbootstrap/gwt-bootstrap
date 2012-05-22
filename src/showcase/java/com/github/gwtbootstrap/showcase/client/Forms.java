3c7ac96506e2a3eafb583f625f9d36064e1d861f
soundTricker <keisuke.oohashi@gmail.com>
2012-05-22 22:29:48 +0900

    add Form Submit Showcase

--- src/showcase/java/com/github/gwtbootstrap/showcase/client/Forms.java
+++ src/showcase/java/com/github/gwtbootstrap/showcase/client/Forms.java
@@ -1,10 +1,16 @@
 package com.github.gwtbootstrap.showcase.client;
 
 import com.github.gwtbootstrap.client.ui.CodeBlock;
+import com.github.gwtbootstrap.client.ui.Form.SubmitCompleteEvent;
+import com.github.gwtbootstrap.client.ui.Form.SubmitEvent;
+import com.github.gwtbootstrap.client.ui.ListBox;
+import com.github.gwtbootstrap.client.ui.TextBox;
 import com.github.gwtbootstrap.showcase.client.resources.CustomResources;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -17,6 +23,8 @@ public class Forms extends Composite {
 	@UiField CodeBlock searchFormCode;
 	@UiField CodeBlock inlineFormCode;
 	@UiField CodeBlock horizontalFormCode;
+	@UiField TextBox name;
+	@UiField ListBox example;
 	
 	interface FormsUiBinder extends UiBinder<Widget, Forms> {
 	}
@@ -28,5 +36,20 @@ public class Forms extends Composite {
 		inlineFormCode.setHTML(CustomResources.RESOURCES.inlineFormCode().getText());
 		horizontalFormCode.setHTML(CustomResources.RESOURCES.horizontalFormCode().getText());
 	}
+	
+	@UiHandler("submitExampleForm")
+	public void onSubmitForm(SubmitEvent e) {
+		
+		String show = "user name: " + name.getValue() + "\n favorite: " + example.getValue();
+		
+		Window.alert(show);
+		
+	}
+	
+	@UiHandler("submitExampleForm")
+	public void onSubmitCompleteForm(SubmitCompleteEvent e) {
+		Window.alert("complete");
+	}
+	
 
 }
