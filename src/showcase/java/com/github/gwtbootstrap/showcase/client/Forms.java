3a35031ab499fc3e9c33fafedb5fff92d174fcf9
soundTricker <keisuke.oohashi@gmail.com>
2012-05-26 17:42:25 +0900

    ControlGroupType showcase is more effective!

--- src/showcase/java/com/github/gwtbootstrap/showcase/client/Forms.java
+++ src/showcase/java/com/github/gwtbootstrap/showcase/client/Forms.java
@@ -1,13 +1,27 @@
 package com.github.gwtbootstrap.showcase.client;
 
+import com.github.gwtbootstrap.client.ui.Button;
+import com.github.gwtbootstrap.client.ui.ButtonGroup;
+import com.github.gwtbootstrap.client.ui.CellTable;
+import com.github.gwtbootstrap.client.ui.CheckBox;
 import com.github.gwtbootstrap.client.ui.CodeBlock;
+import com.github.gwtbootstrap.client.ui.ControlGroup;
+import com.github.gwtbootstrap.client.ui.Controls;
+import com.github.gwtbootstrap.client.ui.Fieldset;
+import com.github.gwtbootstrap.client.ui.FileUpload;
 import com.github.gwtbootstrap.client.ui.Form.SubmitCompleteEvent;
 import com.github.gwtbootstrap.client.ui.Form.SubmitEvent;
 import com.github.gwtbootstrap.client.ui.ListBox;
+import com.github.gwtbootstrap.client.ui.PasswordTextBox;
+import com.github.gwtbootstrap.client.ui.RadioButton;
+import com.github.gwtbootstrap.client.ui.TextArea;
 import com.github.gwtbootstrap.client.ui.TextBox;
-import com.github.gwtbootstrap.client.ui.incubator.CellTable;
+import com.github.gwtbootstrap.client.ui.constants.ButtonType;
+import com.github.gwtbootstrap.client.ui.constants.ControlGroupType;
 import com.github.gwtbootstrap.showcase.client.resources.CustomResources;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -29,6 +43,20 @@ public class Forms extends Composite {
 	@UiField CodeBlock horizontalFormCode;
 	@UiField TextBox name;
 	@UiField ListBox example;
+	
+	
+	@UiField ButtonGroup controlGroupButtons;
+	@UiField Fieldset controlFieldset;
+	@UiField TextBox controlText;
+	@UiField PasswordTextBox controlPasswordTextBox;
+	@UiField FileUpload controlFileUpload;
+	@UiField TextArea controlTextarea;
+	@UiField CheckBox controlCheckbox;
+	@UiField Controls controlRadioControls;
+	@UiField ListBox controlList;
+	
+	
+	
 	@UiField(provided=true) CellTable<Sample> exampleTable;
 	ListDataProvider<Sample> dataProvider = new ListDataProvider<Forms.Sample>();
 	
@@ -45,6 +73,54 @@ public class Forms extends Composite {
 		inlineFormCode.setHTML(CustomResources.RESOURCES.inlineFormCode().getText());
 		horizontalFormCode.setHTML(CustomResources.RESOURCES.horizontalFormCode().getText());
 		dataProvider.addDataDisplay(exampleTable);
+		
+
+		initControlButtonGroups();
+	}
+
+	private void initControlButtonGroups() {
+		for (final ControlGroupType type : ControlGroupType.values()) {
+			Button controlGroupButton = new Button(type.name());
+			
+			switch (type) {
+			case ERROR:
+				controlGroupButton.setType(ButtonType.DANGER);
+				break;
+			case SUCCESS:
+				controlGroupButton.setType(ButtonType.INFO);
+				break;
+			case WARNING:
+				controlGroupButton.setType(ButtonType.WARNING);
+				break;
+			case NONE:
+				controlGroupButton.setType(ButtonType.DEFAULT);
+			default:
+				break;
+			}
+			
+			
+			
+			controlGroupButton.addClickHandler(new ClickHandler() {
+				
+				@Override
+				public void onClick(ClickEvent event) {
+					
+					for(Widget w : controlFieldset) {
+						if (w instanceof ControlGroup) {
+							ControlGroup cg = (ControlGroup) w;
+							
+							cg.setType(type);
+							
+						}
+					}
+					
+				}
+			});
+			
+			controlGroupButtons.add(controlGroupButton);
+		}
+		
+		
 	}
 	
 	@UiHandler("submitExampleForm")
@@ -113,4 +189,29 @@ public class Forms extends Composite {
 		}
 	}
 	
+	
+	@UiHandler({"enableButton" , "disableButton"})
+	public void onDisabledButtonClick(ClickEvent c) {
+		
+		Button b = (Button)c.getSource();
+		
+		String text = b.getText();
+		
+		boolean enable = text.equals("Enable");
+		controlText.setEnabled(enable);
+		controlPasswordTextBox.setEnabled(enable);
+		controlTextarea.setEnabled(enable);
+		controlFileUpload.setEnabled(enable);
+		controlCheckbox.setEnabled(enable);
+		for (Widget widget : controlRadioControls) {
+			
+			if (widget instanceof RadioButton) {
+				RadioButton radio = (RadioButton) widget;
+				radio.setEnabled(enable);
+			}
+		}
+		
+		controlList.setEnabled(enable);
+		
+	}
 }
