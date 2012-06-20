80cafdb1abe2eb9931d2c8f76f95312183221507
soundTricker <keisuke.oohashi@gmail.com>
2012-06-20 22:13:24 +0900

    #167

--- src/main/java/com/github/gwtbootstrap/client/ui/base/DropdownBase.java
+++ src/main/java/com/github/gwtbootstrap/client/ui/base/DropdownBase.java
@@ -17,10 +17,9 @@ package com.github.gwtbootstrap.client.ui.base;
 
 import com.github.gwtbootstrap.client.ui.NavLink;
 import com.github.gwtbootstrap.client.ui.constants.Constants;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.event.dom.client.ChangeEvent;
-import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.DomEvent;
@@ -42,153 +41,163 @@ import com.google.gwt.user.client.ui.Widget;
 //@formatter:on
 public abstract class DropdownBase extends ComplexWidget implements HasClickHandlers, HasWidgets, HasText {
 
-    private UnorderedList menu = new UnorderedList();
+	private UnorderedList menu = new UnorderedList();
 
-    private IconAnchor trigger;
+	private IconAnchor trigger;
 
-    private NavLink link;
+	private NavLink link;
 
-    private NavLinkClickHandler handler = new NavLinkClickHandler();
+	private NavLinkClickHandler handler = new NavLinkClickHandler();
 
-    /**
-     * Creates a new widget of the given type.
-     *
-     * @param type the HTML tag to be used for the widget
-     */
-    public DropdownBase(String type) {
-        super(type);
-        createAndAddTrigger();
-        menu.setStyleName("dropdown-menu");
-        super.add(menu);
-    }
+	/**
+	 * Creates a new widget of the given type.
+	 * 
+	 * @param type
+	 *            the HTML tag to be used for the widget
+	 */
+	public DropdownBase(String type) {
+		super(type);
+		createAndAddTrigger();
+		menu.setStyleName("dropdown-menu");
+		super.add(menu);
+	}
 
-    private void createAndAddTrigger() {
-        trigger = createTrigger();
-        trigger.addStyleName("dropdown-toggle");
-        trigger.getElement().setAttribute(Constants.DATA_TOGGLE, "dropdown");
-        super.add(trigger);
-    }
+	private void createAndAddTrigger() {
+		trigger = createTrigger();
+		trigger.addStyleName("dropdown-toggle");
+		trigger.getElement().setAttribute(Constants.DATA_TOGGLE, "dropdown");
+		super.add(trigger);
+	}
 
-    /**
-     * Sets the text of the dropdown trigger.
-     *
-     * @param text
-     */
-    public void setText(String text) {
-        trigger.setText(text);
-    }
-    
-    /**
-     * @return the text of the dropdown trigger.
-     */
-    public String getText() {
-        return trigger.getText();
-    }
+	/**
+	 * Sets the text of the dropdown trigger.
+	 * 
+	 * @param text
+	 */
+	public void setText(String text) {
+		trigger.setText(text);
+	}
 
-    /**
-     * Implement this method to create the trigger appropriate for your widget.
-     * It has to be an {@link IconAnchor} or a subtype.
-     *
-     * @return the created trigger
-     */
-    protected abstract IconAnchor createTrigger();
+	/**
+	 * @return the text of the dropdown trigger.
+	 */
+	public String getText() {
+		return trigger.getText();
+	}
 
-    /**
-     * Set dropup style.
-     * @param dropup true:Set Dropup false:Un-set Dropup 
-     */
-    public void setDropup(boolean dropup) {
-        if (dropup)
-            addStyleName(Constants.DROPUP);
-        else
-            removeStyleName(Constants.DROPUP);
-    }
+	/**
+	 * Implement this method to create the trigger appropriate for your widget.
+	 * It has to be an {@link IconAnchor} or a subtype.
+	 * 
+	 * @return the created trigger
+	 */
+	protected abstract IconAnchor createTrigger();
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void onLoad() {
-        super.onLoad();
-        if (trigger != null) {
-            configure(trigger.getElement());
-        }
-    }
+	/**
+	 * Set dropup style.
+	 * 
+	 * @param dropup
+	 *            true:Set Dropup false:Un-set Dropup
+	 */
+	public void setDropup(boolean dropup) {
+		if (dropup)
+			addStyleName(Constants.DROPUP);
+		else
+			removeStyleName(Constants.DROPUP);
+	}
 
-    /**
-     * Adds a widget to the dropdown menu.
-     *
-     * @param widget the widget that will be added to the menu
-     * @see #addWidget(Widget)
-     */
-    @Override
-    public void add(Widget widget) {
-        menu.add(widget);
-        if (widget instanceof NavLink) {
-            ((NavLink) widget).addClickHandler(handler);
-        }
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void onLoad() {
+		super.onLoad();
+		if (trigger != null) {
+			configure(trigger.getElement());
+		}
+	}
 
-    /**
-     * Adds a widget to the the dropdown widget, <b>not</b> to the dropdown
-     * menu.
-     * <p/>
-     * <p/>
-     * Use {@link #add(Widget)} if you want to add a widget to the dropdown
-     * menu.
-     *
-     * @param widget the widget to be added.
-     */
-    protected void addWidget(Widget widget) {
-        super.add(widget);
-    }
+	/**
+	 * Adds a widget to the dropdown menu.
+	 * 
+	 * @param widget
+	 *            the widget that will be added to the menu
+	 * @see #addWidget(Widget)
+	 */
+	@Override
+	public void add(Widget widget) {
+		menu.add(widget);
+		if (widget instanceof NavLink) {
+			((NavLink) widget).addClickHandler(handler);
+		}
+	}
 
-    private native void configure(Element e) /*-{
-        $wnd.jQuery(e).dropdown();
-    }-*/;
+	/**
+	 * Adds a widget to the the dropdown widget, <b>not</b> to the dropdown
+	 * menu.
+	 * <p/>
+	 * <p/>
+	 * Use {@link #add(Widget)} if you want to add a widget to the dropdown
+	 * menu.
+	 * 
+	 * @param widget
+	 *            the widget to be added.
+	 */
+	protected void addWidget(Widget widget) {
+		super.add(widget);
+	}
 
-    /**
-     * Pull the dropdown menu to right
-     *
-     * @param rightDropdown <code>true</code> pull to right, otherwise to left. Default is <code>false</code>
-     */
-    public void setRightDropdown(boolean rightDropdown) {
-        if (rightDropdown) {
-            menu.addStyleName("pull-right");
-        } else {
-            menu.removeStyleName("pull-right");
-        }
-    }
+	private native void configure(Element e) /*-{
+												$wnd.jQuery(e).dropdown();
+												}-*/;
 
-    public HandlerRegistration addChangeHandler(ChangeHandler handler) {
-        return addDomHandler(handler, ChangeEvent.getType());
-    }
+	/**
+	 * Pull the dropdown menu to right
+	 * 
+	 * @param rightDropdown
+	 *            <code>true</code> pull to right, otherwise to left. Default is
+	 *            <code>false</code>
+	 */
+	public void setRightDropdown(boolean rightDropdown) {
+		if (rightDropdown) {
+			menu.addStyleName("pull-right");
+		} else {
+			menu.removeStyleName("pull-right");
+		}
+	}
 
-    private class NavLinkClickHandler implements ClickHandler {
+	private class NavLinkClickHandler implements ClickHandler {
 
-        @Override
-        public void onClick(ClickEvent event) {
-            DomEvent.fireNativeEvent(Document.get().createChangeEvent(), DropdownBase.this);
-        }
+		@Override
+		public void onClick(ClickEvent event) {
+			try {
+				IconAnchor iconAnchor = (IconAnchor) event.getSource();
+				link = (NavLink) iconAnchor.getParent();
+			} catch (Exception e) {
+				GWT.log(e.getMessage() , e);
+			}
+			DomEvent.fireNativeEvent(Document.get().createChangeEvent(), DropdownBase.this);
+		}
 
-    }
+	}
 
-    /**
-     * Method to get the {@link NavLink} that has been clicked most recently.
-     *
-     * @return Last clicked NavLink or <code>null</code> if none have been clicked.
-     */
-    public NavLink getLastSelectedNavLink() {
-        return link;
-    }
+	/**
+	 * Method to get the {@link NavLink} that has been clicked most recently.
+	 * 
+	 * @return Last clicked NavLink or <code>null</code> if none have been
+	 *         clicked.
+	 */
+	public NavLink getLastSelectedNavLink() {
+		return link;
+	}
 
-    @Override
-    public void clear() {
-        menu.clear();
-    }
+	@Override
+	public void clear() {
+		menu.clear();
+	}
 
-    @Override
-    public HandlerRegistration addClickHandler(ClickHandler handler) {
-        return trigger.addClickHandler(handler);
-    }
+	@Override
+	public HandlerRegistration addClickHandler(ClickHandler handler) {
+		return trigger.addClickHandler(handler);
+	}
 }
