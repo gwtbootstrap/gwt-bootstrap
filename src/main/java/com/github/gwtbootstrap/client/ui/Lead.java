1c913b064cc9b18506430ae2fc729e73d5aed875
soundTricker <keisuke.oohashi@gmail.com>
2012-08-21 21:21:32 +0900

    Add Lead widget (thin Paragraph wrapper)

---
+++ src/main/java/com/github/gwtbootstrap/client/ui/Lead.java
@@ -0,0 +1,23 @@
+package com.github.gwtbootstrap.client.ui;
+
+import com.github.gwtbootstrap.client.ui.constants.Constants;
+
+/**
+ * Lead
+ * 
+ * @since 2.1.0.0
+ * @author ohashi keisuke
+ *
+ */
+public class Lead extends Paragraph {
+
+    public Lead() {
+        this("");
+    }
+
+    public Lead(String html) {
+        super();
+        addStyleName(Constants.LEAD);
+    }
+    
+}
