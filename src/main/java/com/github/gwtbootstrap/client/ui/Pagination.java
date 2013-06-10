74fb55a38ce398089820c6d3b1370f3a7c553e89
Danilo Reinert <daniloreinert@gmail.com>
2013-06-10 17:38:44 -0300

    Pagination upgrade and NumberedPager implementation

--- src/main/java/com/github/gwtbootstrap/client/ui/Pagination.java
+++ src/main/java/com/github/gwtbootstrap/client/ui/Pagination.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2012 GWT-Bootstrap
+ *  Copyright 2013 GWT-Bootstrap
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -21,30 +21,102 @@ import com.github.gwtbootstrap.client.ui.base.StyleHelper;
 import com.github.gwtbootstrap.client.ui.base.UnorderedList;
 import com.github.gwtbootstrap.client.ui.resources.Bootstrap;
 import com.google.gwt.user.client.ui.Widget;
+import com.github.gwtbootstrap.client.ui.base.*;
+import com.github.gwtbootstrap.client.ui.constants.Device;
+import com.google.gwt.user.client.ui.*;
+
+import java.util.Iterator;
 
 //@formatter:off
 /**
  * Multi-Page Pagination
- * 
+ *
  * @since 2.0.4.0
  * @author Dominik Mayer
+ * @author Danilo Reinert
  */
 //@formatter:on
-public class Pagination extends DivWidget {
+public class Pagination extends Composite implements HasStyle, IsResponsive, HasId, InsertPanel.ForIsWidget, HasWidgets.ForIsWidget {
 
-	private UnorderedList list = new UnorderedList();
+    private final DivWidget paginationDiv = new DivWidget();;
+    private UnorderedList list = new UnorderedList();
 
-	public Pagination() {
-		setStyle(Bootstrap.Pagination.LEFT);
-		super.add(list);
-		setSize(PaginationSize.NORMAL);
-	}
-	
-	public static enum PaginationSize implements Style {
-	    MINI,
-	    SMALL,
-	    NORMAL,
-	    LARGE;
+    public Pagination() {
+        paginationDiv.setStyle(Bootstrap.Pagination.LEFT);
+        paginationDiv.add(list);
+        setSize(PaginationSize.NORMAL);
+        initWidget(paginationDiv);
+    }
+
+    @Override
+    public void add(IsWidget w) {
+        list.add(w);
+    }
+
+    @Override
+    public boolean remove(IsWidget w) {
+        return paginationDiv.remove(w);
+    }
+
+    @Override
+    public void insert(IsWidget w, int beforeIndex) {
+        paginationDiv.insert(w, beforeIndex);
+    }
+
+    @Override
+    public int getWidgetIndex(IsWidget child) {
+        return paginationDiv.getWidgetIndex(child);
+    }
+
+    /**
+     * Gets the child widget at the specified index.
+     *
+     * @param index the child widget's index
+     * @return the child widget
+     */
+    @Override
+    public Widget getWidget(int index) {
+        return list.getWidget(index);
+    }
+
+    /**
+     * Gets the number of child widgets in this panel.
+     *
+     * @return the number of children
+     */
+    @Override
+    public int getWidgetCount() {
+        return list.getWidgetCount();
+    }
+
+    /**
+     * Gets the index of the specified child widget.
+     *
+     * @param child the widget to be found
+     * @return the widget's index, or <code>-1</code> if it is not a child of this
+     *         panel
+     */
+    @Override
+    public int getWidgetIndex(Widget child) {
+        return list.getWidgetIndex(child);
+    }
+
+    /**
+     * Removes the widget at the specified index.
+     *
+     * @param index the index of the widget to be removed
+     * @return <code>false</code> if the widget is not present
+     */
+    @Override
+    public boolean remove(int index) {
+        return list.remove(index);
+    }
+
+    public static enum PaginationSize implements Style {
+        MINI,
+        SMALL,
+        NORMAL,
+        LARGE;
 
         @Override
         public String get() {
@@ -53,28 +125,143 @@ public class Pagination extends DivWidget {
             }
             return "";
         }
-	}
+    }
 
-	public void setAlignment(String alignment) {
-		if (alignment.equalsIgnoreCase("right"))
-			setStyle(Bootstrap.Pagination.RIGHT);
-		else if (alignment.equalsIgnoreCase("centered"))
-			setStyle(Bootstrap.Pagination.CENTERED);
-		else
-			setStyle(Bootstrap.Pagination.LEFT);
-	}
-	
-	public void setSize(PaginationSize size) {
-	    StyleHelper.changeStyle(this, size, PaginationSize.class);
-	}
+    public void setAlignment(String alignment) {
+        if (alignment.equalsIgnoreCase("right"))
+            setStyle(Bootstrap.Pagination.RIGHT);
+        else if (alignment.equalsIgnoreCase("centered"))
+            setStyle(Bootstrap.Pagination.CENTERED);
+        else
+            setStyle(Bootstrap.Pagination.LEFT);
+    }
 
-	@Override
-	public void add(Widget child) {
-		list.add(child);
-	}
-	
-	@Override
-	public void clear() {
-		list.clear();
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public void setId(String id) {
+        paginationDiv.setId(id);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getId() {
+        return paginationDiv.getId();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void setHideOn(Device device) {
+        paginationDiv.setHideOn(device);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void setShowOn(Device device) {
+        paginationDiv.setShowOn(device);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void removeStyle(Style style) {
+        paginationDiv.removeStyle(style);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void addStyle(Style style) {
+        paginationDiv.addStyle(style);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void setStyle(Style style) {
+        paginationDiv.setStyle(style);
+    }
+
+    /**
+     * Pulls the widget to the right side.
+     *
+     * @param pullRight
+     *            <code>true</code> if the widget should be aligned right.
+     */
+    public void pullRight(boolean pullRight) {
+        paginationDiv.pullRight(pullRight);
+    }
+
+    public void setSize(PaginationSize size) {
+        StyleHelper.changeStyle(paginationDiv, size, PaginationSize.class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void add(Widget w) {
+        list.add(w);
+    }
+
+    /**
+     * Removes all child widgets.
+     */
+    @Override
+    public void clear() {
+        list.clear();
+    }
+
+    /**
+     * Gets an iterator for the contained widgets. This iterator is required to
+     * implement {@link java.util.Iterator#remove()}.
+     */
+    @Override
+    public Iterator<Widget> iterator() {
+        return list.iterator();
+    }
+
+    /**
+     * Removes a child widget.
+     *
+     * @param w the widget to be removed
+     * @return <code>true</code> if the widget was present
+     */
+    @Override
+    public boolean remove(Widget w) {
+        return list.remove(w);
+    }
+
+    /**
+     * Inserts another widget into this one.
+     *
+     * @param w
+     *            the widget to be inserted
+     * @param beforeIndex
+     *            the index of the position before which it should be set
+     */
+    public void insert(Widget w, int beforeIndex) {
+        list.insert(w, beforeIndex);
+    }
+
+    public NavLink addPageLink(int page) {
+        final NavLink pageLink = new NavLink(String.valueOf(page));
+        insert(pageLink, page);
+        return pageLink;
+    }
+
+    public NavLink addPreviousLink() {
+        final NavLink pageLink = new NavLink("<");
+        insert(pageLink, 0);
+        return pageLink;
+    }
+
+    public NavLink addNextLink() {
+        final NavLink pageLink = new NavLink(">");
+        add(pageLink);
+        return pageLink;
+    }
+
 }
