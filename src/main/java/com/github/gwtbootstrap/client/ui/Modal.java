0264593bed14fd946a2130698fcf5377bce0ec64
Danilo Reinert <daniloreinert@gmail.com>
2013-06-18 12:55:45 -0300

    fixed some copyright mess

--- src/main/java/com/github/gwtbootstrap/client/ui/Modal.java
+++ src/main/java/com/github/gwtbootstrap/client/ui/Modal.java
@@ -1,14 +1,14 @@
 /*
- *  Copyright 2012 GWT-Bootstrap
+ *  Copyright 2013 GWT-Bootstrap
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance $ese.
- *  You may obtain a$e
- *      http://www.$e$e2.0
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required$e law or agreed to in writing, software
+ *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARR$eITIONS OF ANY KIND, either express or implied.
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
