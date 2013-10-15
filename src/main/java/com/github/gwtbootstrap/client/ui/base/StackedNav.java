74f8534a325123a0d3644e6ca40c395c47369c80
Halil Karakose <halilkarakose@gmail.com>
2013-10-16 00:08:24 +0300

    corrected url of twitter bootstrap navs documentation

--- src/main/java/com/github/gwtbootstrap/client/ui/base/StackedNav.java
+++ src/main/java/com/github/gwtbootstrap/client/ui/base/StackedNav.java
@@ -25,7 +25,7 @@ import com.github.gwtbootstrap.client.ui.resources.Bootstrap;
  * 
  * @author Dominik Mayer
  * 
- * @see <a href="http://twitter.github.com/bootstrap/components.html#navs">Bootstrap documentation</a>
+ * @see <a href="http://getbootstrap.com/2.3.2/components.html#navs">Bootstrap documentation</a>
  */
 //@formatter:on
 public abstract class StackedNav extends UnorderedList {
